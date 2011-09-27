7be50fbfa64d63857cca7e562ad63547438561fe
Chris Lehmann <clehmann@picagroup.com>
2011-09-27 12:28:57 -0500

    Registration now working

--- src/main/java/net/chrislehmann/sipservice/SipServiceContants.java
+++ src/main/java/net/chrislehmann/sipservice/SipServiceContants.java
@@ -4,5 +4,12 @@ public class SipServiceContants {
     public static class Actions {
         public static final String STOP_SERVICE = "net.chrislehmann.sipservice.ACTION_STOP";
         public static final String HANG_UP = "net.chrislehmann.sipservice.ACTION_HANG_UP";
+        public static final String SET_SERVER_INFO = "net.chrislehmann.sipservice.ACTION_SET_SERVER_INFO";
+    }
+
+    public class Extras {
+        public static final String USERNAME_KEY = "username";
+        public static final String PASSWORD = "password";
+        public static final String DOMAIN = "domain";
     }
 }
--- src/main/java/net/chrislehmann/sipservice/service/SipService.java
+++ src/main/java/net/chrislehmann/sipservice/service/SipService.java
@@ -1,48 +1,97 @@
 package net.chrislehmann.sipservice.service;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.widget.Toast;
+import net.chrislehmann.common.util.PreferencesUtilities;
+import net.chrislehmann.linphone.R;
 import net.chrislehmann.sipservice.SipServiceContants;
 import org.linphone.LinphoneManager;
+import org.linphone.LinphoneService;
+import org.linphone.core.Hacks;
 import org.linphone.core.LinphoneCall;
 import org.linphone.core.LinphoneCore;
+import org.linphone.core.LinphoneCoreException;
 import roboguice.service.RoboService;
 import roboguice.util.Ln;
 
 public class SipService extends RoboService implements LinphoneManager.LinphoneServiceListener {
 
-
+    private final static String LOGTAG = LinphoneService.class.getSimpleName();
     private LinphoneManager linphoneManager;
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Ln.d("Got start command");
 
-        if (linphoneManager == null) {
-            linphoneManager = LinphoneManager.createAndStart(getBaseContext(), this);
-        }
-
 
         String action = intent.getAction();
         if (SipServiceContants.Actions.STOP_SERVICE.equals(action)) {
             stopLinphoneManager();
+            stopSelf();
         } else if (SipServiceContants.Actions.HANG_UP.equals(action)) {
+            startLinphoneManage();
             linphoneManager.terminateCall();
+        } else if (SipServiceContants.Actions.SET_SERVER_INFO.equals(action)) {
+            Ln.d("Setting server info");
+            PreferencesUtilities.savePreference(getBaseContext(), R.string.pref_username_key, intent.getStringExtra(SipServiceContants.Extras.USERNAME_KEY));
+            PreferencesUtilities.savePreference(getBaseContext(), R.string.pref_passwd_key, intent.getStringExtra(SipServiceContants.Extras.PASSWORD));
+            PreferencesUtilities.savePreference(getBaseContext(), R.string.pref_domain_key, intent.getStringExtra(SipServiceContants.Extras.DOMAIN));
+
+            stopLinphoneManager();
+            startLinphoneManage();
         }
 
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+
+    private void startLinphoneManage() {
+        if (linphoneManager == null) {
+            Ln.d("Startiing linphone service...");
+            linphoneManager = LinphoneManager.createAndStart(getBaseContext(), this);
+            LinphoneManager.getLc().setNetworkReachable(true);
+        }
+    }
+
+    private void dumpInstalledLinphoneInformation() {
+        PackageInfo info = null;
+        try {
+            info = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException nnfe) {
+        }
+
+        if (info != null) {
+            org.linphone.core.Log.i("Linphone version is ", info.versionCode);
+        } else {
+            org.linphone.core.Log.i("Linphone version is unknown");
+        }
     }
 
     private void stopLinphoneManager() {
-        stopSelf();
-        linphoneManager = null;
-        LinphoneManager.destroy(this);
+//        stopSelf();
+        if (linphoneManager != null) {
+            Ln.d("Stopping linphone service...");
+
+            linphoneManager = null;
+            LinphoneManager.destroy(this);
+        }
     }
 
     @Override
     public void onCreate() {
+        // Set default preferences
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, true);
+
+        // Dump some debugging information to the logs
+        Hacks.dumpDeviceInformation();
+        dumpInstalledLinphoneInformation();
+
         Toast.makeText(this, "Started Service....", Toast.LENGTH_SHORT).show();
         super.onCreate();
     }
@@ -54,6 +103,8 @@ public class SipService extends RoboService implements LinphoneManager.LinphoneS
     }
 
     public void onGlobalStateChanged(LinphoneCore.GlobalState state, String message) {
+
+        Log.d(LOGTAG, "Global state changed: " + state + " : " + message);
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
@@ -70,10 +121,22 @@ public class SipService extends RoboService implements LinphoneManager.LinphoneS
     }
 
     public void onRegistrationStateChanged(LinphoneCore.RegistrationState state, String message) {
+        Log.d(LOGTAG, "Registration state changed: " + state + " : " + message);
+//        LinphoneManager.getInstance()
+
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
     public void onCallStateChanged(LinphoneCall call, LinphoneCall.State state, String message) {
+
+        try {
+            LinphoneManager.getInstance().acceptCallIfIncomingPending();
+        } catch (LinphoneCoreException e) {
+            throw new RuntimeException(e);
+        }
+
+        Log.d(LOGTAG, "Call state changed: " + state + " : " + message);
+
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
